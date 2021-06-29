0ff86728999c1a524325fe26e54cc7d41d207d34
flavia-thurler <flavia.thurler@gmail.com>
2021-06-29 19:47:04 -0300

    ALterações Local Storage

--- src/Controller/SessionController.js
+++ src/Controller/SessionController.js
@@ -4,6 +4,8 @@ module.exports = {
     create(req, res){
         const {id} = req.body;
         console.log(id);
+        res = id;
+        return res.json(id);
         //let busca = data.findOne({id})
     }
 }
